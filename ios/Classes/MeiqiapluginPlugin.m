143d2ef7b3e3a8112585aed677f68ddb2e4d3dac
Puppet <zhengjm@ewhale.cn>
2020-04-17 11:21:33 +0800

    first commit

---
+++ ios/Classes/MeiqiapluginPlugin.m
@@ -0,0 +1,20 @@
+#import "MeiqiapluginPlugin.h"
+
+@implementation MeiqiapluginPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"meiqiaplugin"
+            binaryMessenger:[registrar messenger]];
+  MeiqiapluginPlugin* instance = [[MeiqiapluginPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
