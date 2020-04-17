612a150ba03ced192b35899218c02cdd37d0b1ce
EchoPuda <513219857@qq.com>
2020-04-17 13:20:51 +0800

    美洽Android，ios端

---
+++ android/src/main/java/com/ewhale/meiqiaplugin/MeiQiaHandler.java
@@ -0,0 +1,38 @@
+package com.ewhale.meiqiaplugin;
+
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.meiqia.meiqiasdk.controller.ControllerImpl;
+import com.meiqia.meiqiasdk.util.MQConfig;
+import com.meiqia.meiqiasdk.util.MQIntentBuilder;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry;
+
+//import com.meiqia.core.bean.MQEnterpriseConfig;
+
+public class MeiQiaHandler {
+    private static Context mcontext;
+
+
+    public static void setRegistrar(Context context) {
+        mcontext = context;
+    }
+
+
+    public static void openMeiQia(MethodCall call,MethodChannel.Result result){
+        //设置上线参数，可选
+        //MCOnlineConfig onlineConfig = new MCOnlineConfig();
+//启动对话界面
+
+        MQConfig.registerController(new ControllerImpl(mcontext));
+        Intent intent = new MQIntentBuilder(mcontext).build();
+        mcontext.startActivity(intent);
+//        startActivity(intent);
+    }
+
+
+}
